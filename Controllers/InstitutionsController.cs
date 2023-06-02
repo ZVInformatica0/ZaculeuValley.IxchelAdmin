f04dbd177abaed36c991058c8a099088bdf6b389
estuardo8u14 <estuardo8u14@gmail.com>
2023-06-02 15:40:03 -0600

    Breadcrum Facilities Terminado

--- Controllers/InstitutionsController.cs
+++ Controllers/InstitutionsController.cs
@@ -239,8 +239,8 @@ namespace ZaculeuValley.IxchelAdmin.Controllers
             {
                 HttpContext.Session.SetString("InstitutionName", institution.InstitutionName);
             }
-            //return Redirect($"/Home/{id}");
-            return RedirectToAction($"/Home");
+            return Redirect($"/Home");
+            //return RedirectToAction($"/Home");
         }
 
 
