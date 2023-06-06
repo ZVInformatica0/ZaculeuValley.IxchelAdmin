2c99e947b872f0199ca5fa81d3dd8c93f82bf08a
oivanrobles <oivanrobles>
2023-06-06 11:45:42 -0600

    New tables Login models

---
+++ NewModels/Rol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaculeuValley.IxchelAdmin.NewModels;
+
+public partial class Rol
+{
+    public int Idrol { get; set; }
+
+    public string? RolName { get; set; }
+
+    public string? RolDescription { get; set; }
+
+    public virtual ICollection<PermissionRol> PermissionRols { get; set; } = new List<PermissionRol>();
+
+    public virtual ICollection<UserRol> UserRols { get; set; } = new List<UserRol>();
+}
---
+++ NewModels/UserRol.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaculeuValley.IxchelAdmin.NewModels;
+
+public partial class UserRol
+{
+    public int IduserRol { get; set; }
+
+    public int? Iduser { get; set; }
+
+    public int? Idrol { get; set; }
+
+    public virtual Rol? IdrolNavigation { get; set; }
+
+    public virtual User? IduserNavigation { get; set; }
+}
