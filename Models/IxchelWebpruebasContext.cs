3344fc1113a03554bb419d2415bc2d15a25a2785
ZVSoporte <soporte@zaculeuvalley.com>
2023-06-06 11:54:25 -0600

    Update add new models

--- Models/IxchelWebpruebasContext.cs
+++ Models/IxchelWebpruebasContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using ZaculeuValley.IxchelAdmin.Models;
 
+
 namespace ZaculeuValley.IxchelAdmin.Models;
 
 public partial class IxchelWebpruebasContext : DbContext
@@ -34,7 +35,15 @@ public partial class IxchelWebpruebasContext : DbContext
 
     //UPDATE DE NUEVAS TABLAS
 
+    public virtual DbSet<Permission> Permissions { get; set; }
 
+    public virtual DbSet<PermissionRol> PermissionRols { get; set; }
+
+    public virtual DbSet<Rol> Rols { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<UserRol> UserRols { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //quitar luego de aqui la connectionstring
@@ -284,7 +293,88 @@ public partial class IxchelWebpruebasContext : DbContext
 
         //NEW MODELBUILDER TABLAS NUEVAS
 
-       
+        modelBuilder.Entity<Permission>(entity =>
+        {
+            entity.HasKey(e => e.Idpermission);
+
+            entity.ToTable("Permission");
+
+            entity.Property(e => e.Idpermission).HasColumnName("IDPermission");
+            entity.Property(e => e.PermissionDescription)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.PermissionName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<PermissionRol>(entity =>
+        {
+            entity.HasKey(e => e.IdpermissionRol);
+
+            entity.ToTable("PermissionRol");
+
+            entity.Property(e => e.IdpermissionRol).HasColumnName("IDPermissionRol");
+            entity.Property(e => e.Idpermission).HasColumnName("IDPermission");
+            entity.Property(e => e.Idrol).HasColumnName("IDRol");
+
+            entity.HasOne(d => d.IdpermissionNavigation).WithMany(p => p.PermissionRols)
+                .HasForeignKey(d => d.Idpermission)
+                .HasConstraintName("FK_PermissionRol_IdPermission");
+
+            entity.HasOne(d => d.IdrolNavigation).WithMany(p => p.PermissionRols)
+                .HasForeignKey(d => d.Idrol)
+                .HasConstraintName("FK_PermissionRol_IdRol");
+        });
+
+        modelBuilder.Entity<Rol>(entity =>
+        {
+            entity.HasKey(e => e.Idrol);
+
+            entity.ToTable("Rol");
+
+            entity.Property(e => e.Idrol).HasColumnName("IDRol");
+            entity.Property(e => e.RolDescription)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.RolName)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Iduser);
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.Iduser).HasColumnName("IDUser");
+            entity.Property(e => e.UserName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.UserPassword)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<UserRol>(entity =>
+        {
+            entity.HasKey(e => e.IduserRol);
+
+            entity.ToTable("UserRol");
+
+            entity.Property(e => e.IduserRol).HasColumnName("IDUserRol");
+            entity.Property(e => e.Idrol).HasColumnName("IDRol");
+            entity.Property(e => e.Iduser).HasColumnName("IDUser");
+
+            entity.HasOne(d => d.IdrolNavigation).WithMany(p => p.UserRols)
+                .HasForeignKey(d => d.Idrol)
+                .HasConstraintName("FK_UserRol_IDRol");
+
+            entity.HasOne(d => d.IduserNavigation).WithMany(p => p.UserRols)
+                .HasForeignKey(d => d.Iduser)
+                .HasConstraintName("FK_UserRol_IDUser");
+        });
 
 
         OnModelCreatingPartial(modelBuilder);
--- Models/PermissionRol.cs
+++ Models/PermissionRol.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace ZaculeuValley.IxchelAdmin.NewModels;
+namespace ZaculeuValley.IxchelAdmin.Models;
 
 public partial class PermissionRol
 {
