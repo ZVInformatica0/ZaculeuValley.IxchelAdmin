e3c6aa327cad37142340d7cb77178b848dfe692d
ZVSoporte <soporte@zaculeuvalley.com>
2023-06-06 10:58:24 -0600

    Update new tables and models.

---
+++ Models/Permission.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaculeuValley.IxchelAdmin.Models;
+
+public partial class Permission
+{
+    public int Idpermission { get; set; }
+
+    public string? PermissionName { get; set; }
+
+    public string? PermissionDescription { get; set; }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaculeuValley.IxchelAdmin.Models;
+
+public partial class User
+{
+    public int Iduser { get; set; }
+
+    public string UserName { get; set; } = null!;
+
+    public string UserPassword { get; set; } = null!;
+
+    public int? UserState { get; set; }
+
+    public bool UserStateSession { get; set; }
+
+    public bool UserEnable { get; set; }
+
+    public bool? UserDeleted { get; set; }
+
+    public virtual ICollection<UserRol> UserRols { get; set; } = new List<UserRol>();
+}
