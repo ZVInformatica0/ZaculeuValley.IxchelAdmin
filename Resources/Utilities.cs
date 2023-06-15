96a8b56c7de853f37b94bfba98129ac2528077ab
oivanrobles <oivanrobles>
2023-06-15 08:11:46 -0600

    sin login

---
+++ Resources/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace ZaculeuValley.IxchelAdmin.Resources
+{
+    public class Utilities
+    {
+        public static string EncryptPassword(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (SHA256 hash = SHA256Managed.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(password));
+
+                foreach (byte b in result)
+                    sb.Append(b.ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
